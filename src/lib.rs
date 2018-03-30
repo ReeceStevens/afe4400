df78c7826b6f2742c13fbd250dfd58fe4e24e80e
Reece Stevens <reecestevens24@gmail.com>
2018-03-30 01:25:45 -0500

    Use more graceful "?" syntax
    NOTE that the latest packaged version of the `nb` crate doesn't yet
    implement the `From` trait, so the `?` operator is invalid. I manually
    added the `From` trait to verify that it works as expected. This will
    not compile out-of-the-box until `nb` 0.1.12 is released.

--- src/lib.rs
+++ src/lib.rs
@@ -77,74 +77,59 @@ pub struct Afe4400<SPI: FullDuplex<u8>, IN: InputPin, OUT: OutputPin> {
     afe_pdn: OUT
 }
 
-// type SpiError = <core::fmt::Error as FullDuplex<u8>>::Error;
 
-// impl<E> From<E> for nb::Error<E>
-//     where E:  {
-//     fn from(error: E) -> nb::Error<E> {
-//         nb::Error::Other(error)
-//     }
-// }
-
-// impl From<SpiError> for nb::Error<SpiError> {
-//     fn from(error: SpiError) -> Self {
-//         nb::Error::Other(error)
-//     }
-// }
-
-pub enum AfeError {
-    Wrong
-}
-
+type SpiError<SPI> = <SPI as FullDuplex<u8>>::Error;
 
 // TODO Replace these unwraps and unused results with `?` operator once the
 // `nb` crate fix gets released and embedded-hal is updated.
+
 impl<SPI: FullDuplex<u8>, IN: InputPin, OUT: OutputPin> Afe4400<SPI, IN, OUT> {
-    pub fn writeData(&mut self, register: u8, data: u32) -> nb::Result<(), AfeError> {
-        block!(self.spi.send(register));
+    pub fn writeData(&mut self, register: u8, data: u32) -> nb::Result<(), SpiError<SPI>> {
+        block!(self.spi.send(register))?;
         let first_transfer = ((data >> 16) & 0xFF) as u8;
         let second_transfer = ((data >> 8) & 0xFF) as u8;
         let third_transfer = (data & 0xFF) as u8;
-        block!(self.spi.send(first_transfer));
-        block!(self.spi.send(second_transfer));
-        block!(self.spi.send(third_transfer));
+        block!(self.spi.send(first_transfer))?;
+        block!(self.spi.send(second_transfer))?;
+        block!(self.spi.send(third_transfer))?;
         Ok(())
     }
 
-    pub fn readData(&mut self, register: u8) -> nb::Result<u32, AfeError> {
-        self.writeData(registers::CONTROL0, 0x01 as u32).unwrap();
-        block!(self.spi.send(register));
+    pub fn readData(&mut self, register: u8) -> nb::Result<u32, SpiError<SPI>> {
+        self.writeData(registers::CONTROL0, 0x01 as u32);
+        block!(self.spi.send(register))?;
         let mut register_data: u32 = 0;
         for _ in 0..3 {
             register_data = register_data << 8;
-            register_data |= block!(self.spi.read()).ok().unwrap() as u32;
+            register_data |= block!(self.spi.read())? as u32;
         }
         Ok(register_data)
     }
 
-    fn getLedData(&mut self, led_register: u8) -> u32 {
-        let mut value = self.readData(led_register);
-        if (value & 0x00200000) {
+    fn getLedData(&mut self, led_register: u8) -> nb::Result<u32, SpiError<SPI>> {
+        let mut value = self.readData(led_register)?;
+        if value & 0x00200000 != 0 {
             value |= 0xFFD00000;
         }
-        value
+        Ok(value)
     }
 
-    pub fn getLed1Data(&mut self) -> u32 {
+    pub fn getLed1Data(&mut self) -> nb::Result<u32, SpiError<SPI>> {
         self.getLedData(registers::LED1_ALED1VAL)
     }
 
-    pub fn getLed2Data(&mut self) -> u32 {
+    pub fn getLed2Data(&mut self) -> nb::Result<u32, SpiError<SPI>> {
         self.getLedData(registers::LED2_ALED2VAL)
     }
 
     /// TIA_AMB_GAIN: RF_LED[2:0] set to 110
     /// (see p.64 in docs for all Rf options)
-    pub fn setCancellationFilters(&mut self, value: u16) {
-        let mut tia_settings = self.readData(registers::TIA_AMB_GAIN);
-        tia_settings &= ~(0x07);
-        tia_settings |= (value | 0x4400);
-        self.writeData(registers::TIA_AMB_GAIN, tia_settings);
+    pub fn setCancellationFilters(&mut self, value: u16) -> nb::Result<(), SpiError<SPI>> {
+        let mut tia_settings = self.readData(registers::TIA_AMB_GAIN)?;
+        tia_settings &= !(0x07);
+        tia_settings |= (value as u32) | 0x4400;
+        self.writeData(registers::TIA_AMB_GAIN, tia_settings)?;
+        Ok(())
     }
 
     /// LEDCNTRL (0x22)
@@ -154,57 +139,58 @@ impl<SPI: FullDuplex<u8>, IN: InputPin, OUT: OutputPin> Afe4400<SPI, IN, OUT> {
     ///      ------------------  *  50 mA = current
     ///           256
     ///
-    pub fn setLedCurrent(&mut self, value: u8) -> u32 {
-        let both_leds = ((value as u32) << 8) + value;
-        self.writeData(registers::LEDCNTRL, both_leds + 0x010000);
+    pub fn setLedCurrent(&mut self, value: u8) -> nb::Result<u32, SpiError<SPI>> {
+        let both_leds = ((value as u32) << 8) + value as u32;
+        self.writeData(registers::LEDCNTRL, both_leds + 0x010000)?;
         self.readData(registers::LEDCNTRL)
     }
 
-    pub fn defaultPulseTimings(&mut self) {
-        self.writeData(registers::CONTROL0, 0x000000);
-        self.writeData(registers::CONTROL1, 0x0102); // Enable timers
-        self.writeData(registers::CONTROL2, 0x020100);
-        self.writeData(registers::LED2STC, 6050);
-        self.writeData(registers::LED2ENDC, 7998);
-        self.writeData(registers::LED2LEDSTC, 6000);
-        self.writeData(registers::LED2LEDENDC, 7999);
-        self.writeData(registers::ALED2STC, 50);
-        self.writeData(registers::ALED2ENDC, 1998);
-        self.writeData(registers::LED1STC, 2050);
-        self.writeData(registers::LED1ENDC, 3998);
-        self.writeData(registers::LED1LEDSTC, 2000);
-        self.writeData(registers::LED1LEDENDC, 3999);
-        self.writeData(registers::ALED1STC, 4050);
-        self.writeData(registers::ALED1ENDC, 5998);
-        self.writeData(registers::LED2CONVST, 4);
-        self.writeData(registers::LED2CONVEND, 1999);
-        self.writeData(registers::ALED2CONVST, 2004);
-        self.writeData(registers::ALED2CONVEND, 3999);
-        self.writeData(registers::LED1CONVST, 4004);
-        self.writeData(registers::LED1CONVEND, 5999);
-        self.writeData(registers::ALED1CONVST, 6004);
-        self.writeData(registers::ALED1CONVEND, 7999);
-        self.writeData(registers::ADCRSTSTCT0, 0);
-        self.writeData(registers::ADCRSTENDCT0, 3);
-        self.writeData(registers::ADCRSTSTCT1, 2000);
-        self.writeData(registers::ADCRSTENDCT1, 2003);
-        self.writeData(registers::ADCRSTSTCT2, 4000);
-        self.writeData(registers::ADCRSTENDCT2, 4003);
-        self.writeData(registers::ADCRSTSTCT3, 6000);
-        self.writeData(registers::ADCRSTENDCT3, 6003);
-        self.writeData(registers::PRPCOUNT, 7999);
-        self.setCancellationFilters(0x06);
-        self.setLEDCurrent(0x1A);
+    pub fn defaultPulseTimings(&mut self) -> nb::Result<(), SpiError<SPI>> {
+        self.writeData(registers::CONTROL0, 0x000000)?;
+        self.writeData(registers::CONTROL1, 0x0102)?; // Enable timers
+        self.writeData(registers::CONTROL2, 0x020100)?;
+        self.writeData(registers::LED2STC, 6050)?;
+        self.writeData(registers::LED2ENDC, 7998)?;
+        self.writeData(registers::LED2LEDSTC, 6000)?;
+        self.writeData(registers::LED2LEDENDC, 7999)?;
+        self.writeData(registers::ALED2STC, 50)?;
+        self.writeData(registers::ALED2ENDC, 1998)?;
+        self.writeData(registers::LED1STC, 2050)?;
+        self.writeData(registers::LED1ENDC, 3998)?;
+        self.writeData(registers::LED1LEDSTC, 2000)?;
+        self.writeData(registers::LED1LEDENDC, 3999)?;
+        self.writeData(registers::ALED1STC, 4050)?;
+        self.writeData(registers::ALED1ENDC, 5998)?;
+        self.writeData(registers::LED2CONVST, 4)?;
+        self.writeData(registers::LED2CONVEND, 1999)?;
+        self.writeData(registers::ALED2CONVST, 2004)?;
+        self.writeData(registers::ALED2CONVEND, 3999)?;
+        self.writeData(registers::LED1CONVST, 4004)?;
+        self.writeData(registers::LED1CONVEND, 5999)?;
+        self.writeData(registers::ALED1CONVST, 6004)?;
+        self.writeData(registers::ALED1CONVEND, 7999)?;
+        self.writeData(registers::ADCRSTSTCT0, 0)?;
+        self.writeData(registers::ADCRSTENDCT0, 3)?;
+        self.writeData(registers::ADCRSTSTCT1, 2000)?;
+        self.writeData(registers::ADCRSTENDCT1, 2003)?;
+        self.writeData(registers::ADCRSTSTCT2, 4000)?;
+        self.writeData(registers::ADCRSTENDCT2, 4003)?;
+        self.writeData(registers::ADCRSTSTCT3, 6000)?;
+        self.writeData(registers::ADCRSTENDCT3, 6003)?;
+        self.writeData(registers::PRPCOUNT, 7999)?;
+        self.setCancellationFilters(0x06)?;
+        self.setLedCurrent(0x1A)?;
+        Ok(())
     }
 
-    pub fn self_check(&mut self) -> bool {
-        let original_value = self.readData(registers::CONTROL1);
+    pub fn self_check(&mut self) -> nb::Result<bool, SpiError<SPI>> {
+        let original_value = self.readData(registers::CONTROL1)?;
         for _ in 0..5 {
-            if self.readData(registers::CONTROL1) != original_value {
-                return false
+            if self.readData(registers::CONTROL1)? != original_value {
+                return Ok(false)
             }
         }
-        self.readData(registers::CONTROL1) != 0
+        Ok(self.readData(registers::CONTROL1)? != 0)
     }
 
 }
